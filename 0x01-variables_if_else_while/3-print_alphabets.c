9d04cea20b3b897c2ded03cf4ae20efa504b06c0
Youssef Eddaif <jo9unreal@gmail.com>
2023-03-19 18:40:59 +0000

    create 3-print_alphabets.c
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Main program entry point
+ * Return: void
+ */
+int main(void)
+{
+int i;
+for (i = 97; i < 123; i++)
+{
+putchar(i);
+}
+for (i = 65; i < 91; i++)
+{
+putchar(i);
+}
+/* 10 is the ASCII for new line */
+putchar(10);
+return (0);
+}
