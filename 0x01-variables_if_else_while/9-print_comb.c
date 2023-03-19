068f5a8315a8d37bb201a826dbdeb660f5e637fd
Youssef Eddaif <jo9unreal@gmail.com>
2023-03-19 19:05:45 +0000

    create 9-print_comb.c
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Main program entry point
+ * Return: void
+ */
+int main(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+if( i != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+putchar(10);
+return (0);
+}
