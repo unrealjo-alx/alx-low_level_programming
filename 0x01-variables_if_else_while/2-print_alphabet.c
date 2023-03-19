094b69d25c6c76f883c416792e76854b34136849
Youssef Eddaif <jo9unreal@gmail.com>
2023-03-19 18:35:53 +0000

    create 2-print_alphabet.c
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Main program entry point
+ * Return: void
+ */
+int main(void)
+{
+int i;
+for (i = 97; i < 122; i++)
+{
+putchar(i);
+}
+/* 10 is the ASCII for new line */
+putchar(10);
+return (0);
+}
