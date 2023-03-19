ce880a782941d848a84f78d43ddc75fc3e57a746
Youssef Eddaif <jo9unreal@gmail.com>
2023-03-19 18:45:41 +0000

    create 5-print_numbers.c
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Main program entry point
+ * Return: void
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%d",i);
+}
+/* 10 is the ASCII for new line */
+putchar(10);
+return (0);
+}
