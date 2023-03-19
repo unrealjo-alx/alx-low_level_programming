b3cbd6d809bf3e24756e22d77ef42357f0da3dd8
Youssef Eddaif <jo9unreal@gmail.com>
2023-03-19 18:55:26 +0000

    create 6-print_numberz.c
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Main program entry point
+ * Return: void
+ */
+int main(void)
+{
+int i;
+for (i = 48; i < 59; i++)
+{
+putchar(i);
+}
+/* 10 is the ASCII for new line */
+putchar(10);
+return (0);
+}
