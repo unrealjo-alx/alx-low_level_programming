b46a205cb0bca41f7ac0ff46a95244b9644217ea
Youssef Eddaif <jo9unreal@gmail.com>
2023-03-19 18:27:50 +0000

    adding new line at the end of printf statements
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ last_digit = n % 10;
 printf("Last digit of %d is %d ", n, last_digit);
 if (last_digit == 0)
 {
-printf("and is 0");
+printf("and is 0\n");
 }
 else if (n < 6)
 {
-printf("and is less than 6 and not 0");
+printf("and is less than 6 and not 0\n");
 }
 else
 {
-printf("and is greater than 5");
+printf("and is greater than 5\n");
 }
 return (0);
 }
