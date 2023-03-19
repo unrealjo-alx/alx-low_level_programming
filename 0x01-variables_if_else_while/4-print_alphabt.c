dde68fe4f5a06d1de3e351b5d4b1d450956ff790
Youssef Eddaif <jo9unreal@gmail.com>
2023-03-19 18:43:58 +0000

    create 4-print_alphabt.c
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Main program entry point
+ * Return: void
+ */
+int main(void)
+{
+int i;
+for (i = 97; i < 123; i++)
+{
+if( i ==  101 || i == 113) continue;
+putchar(i);
+}
+/* 10 is the ASCII for new line */
+putchar(10);
+return (0);
+}
