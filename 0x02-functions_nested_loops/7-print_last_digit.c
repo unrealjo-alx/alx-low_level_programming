78d3dc89aad86414de2c8a25648f603c8cee10ce
Youssef Eddaif <jo9unreal@gmail.com>
2023-03-21 15:56:29 +0000

    fix 7-print_last_digit.
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,10 +7,9 @@
 int print_last_digit(int n)
 {
 int last_digit;
-if (n > 0)
+if (n < 0)
+n = -n;
 last_digit = n % 10;
-else
-last_digit = (-1 * n) % 10;
 _putchar('0' + last_digit);
 return (last_digit);
 }
