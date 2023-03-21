87b1377e8f9fae93bb5b10f15218de5ea74a30cd
Youssef Eddaif <jo9unreal@gmail.com>
2023-03-21 15:37:21 +0000

    create 6-abs
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer..
+ * @n : an integer
+ * Return: the absolute value of n
+ */
+int _abs(int n)
+{
+if (n < 0)
+return (-1 * n);
+return (n);
+}
