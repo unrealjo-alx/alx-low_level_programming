5c3586accdabb13b93b04bb6dd156ebef7773f28
Youssef Eddaif <jo9unreal@gmail.com>
2023-03-21 14:52:14 +0000

    fix betty style
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - prints the alphabet, in lowercase, followed by a new line.
+ * print_alphabet_x10 - prints the alphabet,
+ * in lowercase, followed by a new line.
  *
  * Return: Void.
  */
 void print_alphabet_x10(void)
 {
-int i,j;
+int i, j;
 for (j = 0; j < 10; j++)
 {
 for (i = 'a'; i < 'z' + 1; i++)
