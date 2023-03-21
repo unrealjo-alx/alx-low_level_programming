d792d9b4af8f3e7b8849d91694876d13b5327f7e
Youssef Eddaif <jo9unreal@gmail.com>
2023-03-21 15:21:21 +0000

    create 5-sign.c
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - function that prints the sign of a number.
+ * @n : an integer
+ * Return: 1 if n is greater than zero,
+ * 0 if n is zero,
+ * -1 if n is less than zero
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);    
+}
+_putchar('0');
+return (0);
+}
