c9f7498cbb845b716be88a07e1df70410ecb47b8
Youssef Eddaif <jo9unreal@gmail.com>
2023-03-22 22:09:52 +0000

    fix 104-fibonacci.c
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,12 +13,12 @@ for (j = 1 ; j <= 98; j++)
 if (j % 2  == 0)
 {
 sequence[1] = sequence[0] + sequence[1];
-printf("%ld", sequence[1] % 1000000000);
+printf("%lu", sequence[1]);
 }
 else
 {
 sequence[0] = sequence[0] + sequence[1];
-printf("%ld", sequence[0] % 1000000000);
+printf("%lu", sequence[0]);
 }
 if (j != 98)
 {
