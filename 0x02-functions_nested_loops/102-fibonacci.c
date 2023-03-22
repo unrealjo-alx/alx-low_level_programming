a80df460801c11131f5d11994c3b81bdb9e61e5a
Youssef Eddaif <jo9unreal@gmail.com>
2023-03-22 21:05:49 +0000

    create 102-fibonacci.c
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints the first 50 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.
+ * Return: the return code
+ */
+int main(void)
+{
+int j, sequence[2] = {0, 1};
+for (j = 1 ; j <= 50; j++)
+{
+if (j % 2  == 0)
+{
+sequence[1] = sequence[0] + sequence[1];
+printf("%d", sequence[1]);
+}
+else
+{
+sequence[0] = sequence[0] + sequence[1];
+printf("%d", sequence[0]);
+}
+if (j != 50)
+{
+printf(", ");
+}
+}
+putchar('\n');
+return (0);
+}
