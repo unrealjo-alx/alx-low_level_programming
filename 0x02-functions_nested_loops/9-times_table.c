357b1e7b5be33fe29fe0af88671b7735be6e952b
Youssef Eddaif <jo9unreal@gmail.com>
2023-03-22 14:06:59 +0000

    create 9-times_table.c
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ * Return: void
+ */
+void times_table(void)
+{
+int i, j, k;
+for (i = 0; i <= 9 ; i++)
+{
+for (j = 0; j <= 9 ; j++)
+{
+k = i * j;
+if (k > 9)
+_putchar('0' + (k / 10));
+else
+_putchar(' ');
+_putchar('0' + (k % 10));
+_putchar(',');
+_putchar(' ');
+}
+_putchar('\n');
+}
+}
