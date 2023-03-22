c33a3cb54e41a36febb8c2734d9ba70d735048e7
Youssef Eddaif <jo9unreal@gmail.com>
2023-03-22 21:56:52 +0000

    trying to fix output result
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #define MAX_VALUE 4000000
-#define EXPECTED_SIZE 10 /* Expected size of results array */
 /**
  * main - prints the first 50 Fibonacci numbers,
  * starting with 1 and 2, followed by a new line.
@@ -8,36 +7,23 @@
  */
 int main(void)
 {
-int j = 0, m = 0, i = 0;
-long int sequence[2] = {0, 1}, results[EXPECTED_SIZE];
+int j = 0, sequence[2] = {0, 1}, result;
 while (sequence[0] <= MAX_VALUE &&  sequence[1] <= MAX_VALUE)
 {
 if (j % 2  == 0)
 {
 sequence[1] = sequence[0] + sequence[1];
-if (sequence[1] % 2 == 0 && sequence[1] != 0)
-{
-results[m] = sequence[1];
-m++;
-}
+if (sequence[1] % 2 == 0 && result <  sequence[1] && sequence[1] != 0)
+result = sequence[1];
 }
 else
 {
 sequence[0] = sequence[0] + sequence[1];
-if (sequence[0] % 2 == 0 && sequence[0] != 0)
-{
-results[m] = sequence[0];
-m++;
-}
+if (sequence[0] % 2 == 0 && result <  sequence[1] && sequence[0] != 0)
+result = sequence[0];
 }
 j++;
 }
-for (; i <= EXPECTED_SIZE; i++)
-{
-printf("%ld", results[i]);
-if (i != EXPECTED_SIZE)
-printf(", ");
-}
-printf("\n");
+printf("%d\n", result);
 return (0);
 }
