6356e9d93d45eeba5f970f3956bebf3ea5a0049e
Youssef Eddaif <jo9unreal@gmail.com>
2023-03-22 13:55:11 +0000

    fix betty styling
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,16 +6,16 @@
  */
 void jack_bauer(void)
 {
-int hour,minute;
-for ( hour = 0; hour <= 23; hour++)
+int hour, minute;
+for (hour = 0; hour <= 23; hour++)
 {
-for ( minute = 0; minute <= 59; minute++)
+for (minute = 0; minute <= 59; minute++)
 {
-_putchar('0'+ (hour / 10));
-_putchar('0'+ (hour % 10));
+_putchar('0' + (hour / 10));
+_putchar('0' + (hour % 10));
 _putchar(':');
-_putchar('0'+ (minute / 10));
-_putchar('0'+ (minute % 10));
+_putchar('0' + (minute / 10));
+_putchar('0' + (minute % 10));
 _putchar('\n');
 }
 }
