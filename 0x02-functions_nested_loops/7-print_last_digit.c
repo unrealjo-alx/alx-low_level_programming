0c4b4116ab37b36668caaf5b0f3996523f4c3553
Youssef Eddaif <jo9unreal@gmail.com>
2023-03-22 13:35:55 +0000

    simple change
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,8 +8,10 @@ int print_last_digit(int n)
 {
 int last_digit;
 if (n < 0)
-n = -n;
+n = -1 * n;
 last_digit = n % 10;
+if (last_digit < 0)
+last_digit = -1 * last_digit;
 _putchar('0' + last_digit);
 return (last_digit);
 }
