ecd0f8957e5fac27fc39fc2e3fbbda6c25c8d4bf
Youssef Eddaif <jo9unreal@gmail.com>
2023-03-23 23:17:42 +0000

    create 9-fizz_buzz.c
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints The "Fizz-Buzz" of the numbers from 1 to 100
+ * Return: void .
+*/
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if ( i % 3 == 0 && i % 5 == 0)
+printf("FizzBuzz");
+else if ( i % 3 == 0)
+printf("Fizz");
+else if ( i % 5 == 0)
+printf("Buzz");
+else
+printf("%d",i);
+if ( i != 100)
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
