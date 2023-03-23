03ab4b1a2d56b622a93e11ff5c00d1a850d8950a
Youssef Eddaif <jo9unreal@gmail.com>
2023-03-23 21:58:48 +0000

    create 0-isupper.c
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+/**
+ * _isupper - checks for uppercase character.
+ * @c : a character code
+ *
+ * Return: 1 if c is uppercase, 0 otherwise .
+*/
+int _isupper(int c)
+{
+return ('A' <= c && c <= 'Z');
+}
