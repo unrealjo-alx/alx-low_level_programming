37adeb2ce6137fd592c93de924b2bf627bb504a2
Youssef Eddaif <jo9unreal@gmail.com>
2023-03-23 22:47:05 +0000

    create 6-print_line.c
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * print_line - draws a straight line in the terminal.
+ * @n : number of line (integer)
+ * Return: void .
+*/
+void print_line(int n)
+{
+for (; n > 0 ; n--)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
