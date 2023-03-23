d492266aa2b36fa0395ef9414dbdf80d9089faae
Youssef Eddaif <jo9unreal@gmail.com>
2023-03-23 22:12:41 +0000

    create 1-isdigit.c
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @c : a character code
+ *
+ * Return: 1 if c is a digit, 0 otherwise .
+*/
+int _isdigit(int c)
+{
+return ('0' <= c && c <= '9');
+}
