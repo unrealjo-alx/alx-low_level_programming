f0c4c754073f18248e0e19a04e32c07a555dc504
Youssef Eddaif <jo9unreal@gmail.com>
2023-03-28 18:05:04 +0000

    create 3-puts.c
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include"main.h"
+#include <stdio.h>
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: a string to print
+ */
+void _puts(char *str)
+{
+int i = 0;
+while (*(str + i) != '\0')
+{
+_putchar(*(str + i));
+i++;
+}
+_putchar('\n');
+}
