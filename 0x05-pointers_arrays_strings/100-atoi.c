2937905257b1b929ffc02f407de493d9b979210e
Youssef Eddaif <jo9unreal@gmail.com>
2023-03-28 23:27:03 +0000

    create 100-atoi.c
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _atoi - convert a string to an integer.
+ * @s: a string
+ * Return: result numbers
+ */
+int _atoi(char *s)
+{
+	int num = 0, i = 0, sign = 1;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == ' ')
+			sign = 1;
+		else if (s[i] == '-')
+			sign = -1;
+
+		if (s[i] >= 48 && s[i] <= 57)
+			num = (s[i] - 48) + num * 10;
+		else if (num > 0)
+			break;
+		i++;
+	}
+
+	return (num * sign);
+}
