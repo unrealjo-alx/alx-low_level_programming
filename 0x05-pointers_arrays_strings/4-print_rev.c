21bc9d09f7e02dea77a7743181b17fc958383eec
Youssef Eddaif <jo9unreal@gmail.com>
2023-03-28 18:10:24 +0000

    create 4-print_rev.c
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include"main.h"
+#include <stdio.h>
+/**
+ * print_rev -  prints a string, in reverse, followed by a new line.
+ * @s: the original string
+ */
+void print_rev(char *s)
+{
+int i = 0;
+while (*(s + i) != '\0')
+i++;
+for (; i >= 0 ; i--)
+_putchar(*(s + i));
+_putchar('\n');
+}
