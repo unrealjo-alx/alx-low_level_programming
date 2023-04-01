5e589a4d7e5e9b0c777e4657103a3a0b38371066
Youssef Eddaif <jo9unreal@gmail.com>
2023-04-01 02:12:18 +0000

    create 100-rot13.c using lookup table
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rot13 - a function that encodes a string using rot13.
+ * @s: the original string
+ * Return: the modified string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char *input, *output;
+	input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 53; j++)
+		{
+			if (s[i] == input[j])
+			{
+				s[i] = output[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
