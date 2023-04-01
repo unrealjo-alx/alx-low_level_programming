a8bcc14b01d78a0d42f5306f4b876233f667c37d
Youssef Eddaif <jo9unreal@gmail.com>
2023-04-01 01:26:47 +0000

    fix #2 for 6-cap_string.c
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,16 +6,28 @@
  */
 char *cap_string(char *s)
 {
-	int i, offset = 'a' - 'A', prev, status;
-
+	int i, offset = 'a' - 'A', prev, status, j;
+	char del[14] = {' ', '\t', '\n',
+					',', ';', '.',
+					':', '!', '?',
+					'"', '(', ')',
+					'{', '}'};
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		prev = i - 1;
 		if (prev < 0)
 			prev = -prev;
+		status = 0;
+		for (j = 0; j < 14; j++)
+		{
+			if (s[prev] == del[j])
+			{
+				status = 1;
+				break;
+			}
+		}
 
-		status = (s[prev] >= 'a' && s[prev] <= 'z') || (s[prev] >= 'A' && s[prev] <= 'Z') || (s[prev] < '0' && s[prev] > '9');
-		if (s[i] >= 'a' && s[i] <= 'z' && !status)
+		if (s[i] >= 'a' && s[i] <= 'z' && status)
 			s[i] = s[i] - offset;
 	}
 	return (s);
