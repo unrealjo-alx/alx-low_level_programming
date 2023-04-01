de327ff3f37e6b0156a10db03278d8f80c5d3421
Youssef Eddaif <jo9unreal@gmail.com>
2023-04-01 01:32:00 +0000

    fix #3 for 6-cap_string.c
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,16 +9,16 @@ char *cap_string(char *s)
 	int i, offset = 'a' - 'A', prev, status, j;
 	char del[14] = {' ', '\t', '\n',
 					',', ';', '.',
-					':', '!', '?',
-					'"', '(', ')',
-					'{', '}'};
+					'!', '?', '"',
+					'(', ')', '{', '}'};
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		prev = i - 1;
 		if (prev < 0)
 			prev = -prev;
 		status = 0;
-		for (j = 0; j < 14; j++)
+		for (j = 0; j < 13; j++)
 		{
 			if (s[prev] == del[j])
 			{
