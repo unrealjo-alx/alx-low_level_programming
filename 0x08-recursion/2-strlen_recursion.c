ae4518b059733b1494da195700ce185063bba532
Youssef Eddaif <jo9unreal@gmail.com>
2023-04-06 21:15:15 +0000

    create 2-strlen_recursion.c
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen_recursion - a function that returns the length of a string.
+ * @s: a string
+ * Return: the length of the string s.
+ */
+int _strlen_recursion(char *s)
+{
+	if (s[0] == '\0')
+		return (0);
+	return (_strlen_recursion(s + 1) + 1);
+}
