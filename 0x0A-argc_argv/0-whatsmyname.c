fb771a6f1ab0d1d467338ee16dd7ab48f2486565
Youssef Eddaif <jo9unreal@gmail.com>
2023-04-10 15:59:12 +0000

    suppress warning of unused parameter

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,9 +5,8 @@
  * @argv: array containing the program command line arguments.
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	if (argc >= 0)
-		printf("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
