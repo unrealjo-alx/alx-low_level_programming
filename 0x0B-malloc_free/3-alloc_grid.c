3dbddb08dd38f2af7c988491da90c7b3571de51f
Youssef Eddaif <jo9unreal@gmail.com>
2023-04-11 22:41:55 +0000

    adjust allocation size for columns

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 	if (width == 0 || height == 0)
 	return (NULL);
 
-	arr = (int **)malloc(width * sizeof(int *));
+	arr = (int **)malloc(height * sizeof(int *));
 
 	if (arr == NULL)
 	{
@@ -22,9 +22,9 @@ int **alloc_grid(int width, int height)
 	return (NULL);
 	}
 
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
-	arr[i] = (int *)malloc(width * sizeof(int *));
+	arr[i] = (int *)malloc(width * sizeof(int));
 
 	if (arr[i] == NULL)
 	{
@@ -32,7 +32,7 @@ int **alloc_grid(int width, int height)
 	return (NULL);
 	}
 
-	for (j = 0; j < height; j++)
+	for (j = 0; j < width; j++)
 	arr[i][j] = 0;
 	}
 
