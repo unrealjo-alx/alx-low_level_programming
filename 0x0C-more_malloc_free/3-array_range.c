8221cdfe52d3d13fdc0af63c9304b44cb6a490f4
Youssef Eddaif <jo9unreal@gmail.com>
2023-04-13 23:01:02 +0000

    replace (min-max) by (max-min) in for loop

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -20,7 +20,7 @@ int *array_range(int min, int max)
 	if (arr == NULL)
 		return (NULL);
 
-	for (i = 0; i <= (min - max); i++)
+	for (i = 0; i < (max - min); i++)
 		arr[i] = min + i;
 
 	return (arr);
