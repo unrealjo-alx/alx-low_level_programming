8ada58a40f7d0e956079ffd8c8a0078ff452d3ca
Youssef Eddaif <jo9unreal@gmail.com>
2023-04-19 15:14:39 +0000

    check for null name of f
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#define NULL (void *)0
 /**
  * print_name - a function that prints a name.
  * @name: a string
@@ -5,5 +6,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
