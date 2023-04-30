36de104e7fbf06c0ea988dcfd54125649f0f24fb
Youssef Eddaif <jo9unreal@gmail.com>
2023-04-30 12:50:02 +0100

    create  4-free_list.c
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - frees a list_t list.
+ * @head: pointer to address of the head of list
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+	while (head != NULL)
+	{
+		tmp = head;
+		free(tmp->str);
+		free(tmp);
+		head = head->next;
+	}
+}
