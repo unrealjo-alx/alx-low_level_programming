46344c347b602365a64112054bc8c1ae1a586578
Youssef Eddaif <jo9unreal@gmail.com>
2023-04-30 20:44:54 +0100

    try a fix
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,7 +22,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	tmp = *head;
 
-	while (i++ < index - 1 && tmp->next != NULL)
+	while (i++ < index - 1 && tmp != NULL)
 		tmp = tmp->next;
 
 	if (tmp == NULL)
