cd03597e50c92761ef4a43ebf17b3f1a0c3e05b4
Youssef Eddaif <jo9unreal@gmail.com>
2023-05-04 22:49:17 +0100

    create 2-get_bit.c
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * get_bit - a function that returns the value
+ * of a bit at a given index.
+ * @n: a positive integer
+ * @index: the index, starting from 0 of the bit you want to get
+ * Return:the value of the bit at given index
+ * or -1 if an error occurred
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int res = -1, i = 0;
+
+	while (n != 0)
+	{
+		if (i == index)
+			res = n % 2;
+		i++;
+		n = n / 2;
+	}
+
+	return (res);
+}
