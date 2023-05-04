511e180ca133fa7d79d994e63eb240d8eb842967
Youssef Eddaif <jo9unreal@gmail.com>
2023-05-04 23:31:36 +0100

    create 5-flip_bits.c
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * flip_bits - a function that returns the number of bits
+ * you would need to flip to get from one number to another.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int num = 0;
+	size_t diff;
+
+	diff = n ^ m;
+
+	while (diff > 0)
+	{
+		num += (diff & 1);
+		diff >>= 1;
+	}
+
+	return (num);
+}
