557a947fb33730469068c5473f4c1b6452143117
Youssef Eddaif <jo9unreal@gmail.com>
2023-05-09 00:30:54 +0100

    create 1-create_file.c
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,4 +6,5 @@
 #define size_t unsigned long int
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #endif
