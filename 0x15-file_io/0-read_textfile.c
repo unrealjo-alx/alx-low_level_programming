746409cf3b652a9f58ceb06b6bfe697c9b8611ed
Youssef Eddaif <jo9unreal@gmail.com>
2023-05-09 00:25:38 +0100

    fixing code style
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,33 +10,35 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    char *buf;
-    ssize_t num_read, num_written;
+	int fd;
+	char *buf;
+	ssize_t num_read, num_written;
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    buf = (char *) malloc(letters + 1);
-    if (!buf) {
-        close(fd);
-        return (0);
-    }
+	buf = (char *)malloc(letters + 1);
+	if (!buf)
+	{
+		close(fd);
+		return (0);
+	}
 
-    num_read = read(fd, buf, letters);
-    if (num_read == -1)
-    {
-        free(buf);
-        close(fd);
-        return (0);
-    }
+	num_read = read(fd, buf, letters);
+	if (num_read == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
 
-    buf[num_read] = '\0';
-    num_written = write(STDOUT_FILENO, buf, num_read);
+	buf[num_read] = '\0';
+	num_written = write(STDOUT_FILENO, buf, num_read);
 
-    free(buf);
-    close(fd);
+	free(buf);
+	close(fd);
 
-    return ((num_written == -1) ? 0 : num_written);
+	return ((num_written == -1) ? 0 : num_written);
 }
+
