ca10f0c2f8b33ec56d7eca45cba712cbdd342cac
Youssef Eddaif <jo9unreal@gmail.com>
2023-05-09 01:10:38 +0100

    split logic into functions for betty style guide
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,51 +5,86 @@
 #include <errno.h>
 
 #define BUFFER_SIZE 1024
+
+void error_exit(char *msg, int code);
+void check_arguments(int argc);
+int open_file(char *filename, int flags, mode_t mode);
+void copy_file(int fd_from, int fd_to);
+void close_file(int fd);
+
 /**
- * error_exit - print error message to STDERR and exit with given code
- * @msg: error message
+ * main - entry point for the program
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ * Return: exit code
+ */
+int main(int argc, char **argv)
+{
+	int fd_from, fd_to;
+
+	check_arguments(argc);
+
+	fd_from = open_file(argv[1], O_RDONLY, 0);
+	fd_to = open_file(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+
+	copy_file(fd_from, fd_to);
+
+	close_file(fd_from);
+	close_file(fd_to);
+
+	return (0);
+}
+
+/**
+ * error_exit - prints an error message to stderr and exits the program
+ * @msg: error message to print
  * @code: exit code
- *
- * Return: nothing
  */
 void error_exit(char *msg, int code)
 {
 	dprintf(STDERR_FILENO, "Error: %s\n", msg);
 	exit(code);
 }
+
 /**
- * main - program entry point
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: 0 on success, or an error code on failure
+ * check_arguments - checks the number of command line arguments
+ * @argc: number of command line arguments
  */
-
-int main(int argc, char **argv)
+void check_arguments(int argc)
 {
-	int fd_from, fd_to, num_read, num_written;
-	char buffer[BUFFER_SIZE];
-
 	if (argc != 3)
 	{
 		error_exit("Usage: file_from file_to", 97);
 	}
+}
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		char msg[1024];
-		snprintf(msg, sizeof(msg), "Can't read from file %s", argv[1]);
-		error_exit(msg, 98);
-	}
+/**
+ * open_file - opens a file with the given filename, flags, and mode
+ * @filename: name of the file to open
+ * @flags: flags to use when opening the file
+ * @mode: mode to use when creating the file
+ * Return: file descriptor of the opened file
+ */
+int open_file(char *filename, int flags, mode_t mode)
+{
+	int fd = open(filename, flags, mode);
 
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	if (fd_to == -1)
+	if (fd == -1)
 	{
-		char msg[1024];
-		snprintf(msg, sizeof(msg), "Can't write to file %s", argv[2]);
-		error_exit(msg, 99);
+		error_exit("Can't read from file", 98);
 	}
+	return (fd);
+}
+
+/**
+ * copy_file - copies the contents of one file to another
+ * @fd_from: file descriptor of the file to read from
+ * @fd_to: file descriptor of the file to write to
+ */
+void copy_file(int fd_from, int fd_to)
+{
+	ssize_t num_read, num_written;
+	char buffer[BUFFER_SIZE];
 
 	while ((num_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
@@ -62,24 +97,19 @@ int main(int argc, char **argv)
 
 	if (num_read == -1)
 	{
-		char msg[1024];
-		snprintf(msg, sizeof(msg), "Can't read from file %s", argv[1]);
-		error_exit(msg, 98);
+		error_exit("Can't read from file", 98);
 	}
+}
 
-	if (close(fd_from) == -1)
+/**
+ * close_file - closes a file
+ * @fd: file descriptor of the file to close
+ */
+void close_file(int fd)
+{
+	if (close(fd) == -1)
 	{
-		char msg[1024];
-		snprintf(msg, sizeof(msg), "Can't close fd %d", fd_from);
-		error_exit(msg, 100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
 	}
-
-	if (close(fd_to) == -1)
-	{
-		char msg[1024];
-		snprintf(msg, sizeof(msg), "Can't close fd %d", fd_to);
-		error_exit(msg, 100);
-	}
-
-	return (0);
 }
